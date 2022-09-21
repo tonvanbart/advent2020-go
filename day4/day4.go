e45ccbffcf14a6e001ac61cfb87aac9a30ed1e3b
Ton van Bart <tonvanbart@gmail.com>
2022-09-21 22:30:57 +0200

    part 2 (fiddly)

--- day4/day4.go
+++ day4/day4.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -16,6 +18,7 @@ func main() {
 	passports := strings.Split(string(input), "\n\n")
 	fmt.Printf("nr of input passports: %d \n", len(passports))
 	validcount := 0
+	validcount2 := 0
 
 	for i := 0; i < len(passports); i++ {
 		// split passport in key:value pairs
@@ -41,6 +44,99 @@ func main() {
 		if byr && iyr && eyr && hgt && hcl && ecl && pid {
 			validcount++
 		}
+
+		// part 2
+		if valid2(fields) {
+			validcount2++
+		}
+
 	}
 	fmt.Printf("Valid passports: %d\n", validcount)
+	fmt.Printf("Out of %d passports %d were valid\n", len(passports), validcount2)
+
+}
+
+func valid2(fields map[string]string) bool {
+	// valid eye colors
+	colors := map[string]bool{
+		"amb": true, "blu": true, "brn": true, "gry": true, "grn": true, "hzl": true, "oth": true,
+	}
+	// check required key presence
+	vbyr, byr := fields["byr"]
+	viyr, iyr := fields["iyr"]
+	veyr, eyr := fields["eyr"]
+	vhgt, hgt := fields["hgt"]
+	vhcl, hcl := fields["hcl"]
+	vecl, ecl := fields["ecl"]
+	vpid, pid := fields["pid"]
+
+	if !(byr && iyr && eyr && hgt && hcl && ecl && pid) {
+		return false
+	}
+
+	birth, err := strconv.Atoi(vbyr)
+	if err != nil || birth < 1920 || birth > 2002 {
+		log.Printf("Birth %d did not match\n", birth)
+		return false
+	}
+
+	issue, err := strconv.Atoi(viyr)
+	if err != nil || issue < 2010 || issue > 2020 {
+		log.Printf("iyr %d wrong\n", issue)
+		return false
+	}
+
+	expire, err := strconv.Atoi(veyr)
+	if err != nil || expire < 2020 || expire > 2030 {
+		log.Printf("eyr %s wrong\n", veyr)
+		return false
+	}
+
+	// check eye color present in valid colors
+	_, found := colors[vecl]
+	if !found {
+		log.Printf("ecl %s wrong\n", vecl)
+		return false
+	}
+
+	// check hair color on regex
+	matched, err := regexp.MatchString("#[0-9a-f]{6}", vhcl)
+	if err != nil || !matched {
+		log.Printf("hcl %s wrong\n", vhcl)
+		return false
+	}
+
+	// check vpid on regexp
+	matchedpid, err := regexp.MatchString("\\d{9}", vpid)
+	if err != nil || !matchedpid {
+		log.Printf("pid %s wrong\n", vpid)
+		return false
+	}
+
+	// match height on regex and keep subgroups for height and cm/in
+	re, err := regexp.Compile(`(\d+)(cm|in)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	parts := re.FindStringSubmatch(vhgt)
+	if len(parts) == 0 {
+		log.Printf("height %s not parsed\n", vhgt)
+		return false
+	} else {
+		height, err := strconv.Atoi(parts[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		if parts[2] == "cm" && (height < 150 || height > 193) {
+			log.Printf("height %s out of bounds\n", vhgt)
+			return false
+		}
+		if parts[2] == "in" && (height < 59 || height > 79) {
+			log.Printf("height %s out of bounds\n", vhgt)
+			return false
+		}
+	}
+
+	return true
+
 }
