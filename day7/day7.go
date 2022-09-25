2848c639b149e8aeffabf977bc231f54cbf5bf24
Ton van Bart <tonvanbart@gmail.com>
2022-09-25 22:28:37 +0200

    rule parsing

--- day7/day7.go
+++ day7/day7.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 )
 
 func main() {
@@ -11,6 +13,33 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("nr of rules %d\n", len(input))
+	rulesin := strings.Split(string(input), "\n")
+	fmt.Printf("nr of rules %d\n", len(rulesin))
+
+	rules := make(map[string]map[string]bool)
+	re, err := regexp.Compile(`(\w+ \w+ bag)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, line := range rulesin {
+		parts := re.FindAllString(line, -1)
+		rules[parts[0]] = makeset(parts[1:])
+	}
+	for bag, containing := range rules {
+		fmt.Printf("bag %s contains: ", bag)
+		for key, _ := range containing {
+			fmt.Printf("%s ", key)
+		}
+		fmt.Println()
+	}
+}
+
+// return a map of string to bool, containing all input strings as keys.
+func makeset(bags []string) map[string]bool {
+	result := make(map[string]bool)
+	for _, bag := range bags {
+		result[bag] = true
+	}
+	return result
 
 }
